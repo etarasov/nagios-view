1ddac1bb790490c5dd357a5409f2688b1757aab5
Evgeny Tarasov <etarasov.ekb@gmail.com>
2014-04-02 16:47:11 +0400

    change default port to 9000 and bind to 0.0.0.0 instead of loopback

--- src/Main.hs
+++ src/Main.hs
@@ -13,12 +13,12 @@ import System.Posix.Syslog
 
 main :: IO ()
 main = do
-    let httpConf = Conf 8080 Nothing Nothing 60 Nothing
+    let httpConf = Conf 9000 Nothing Nothing 60 Nothing
     sock <- socket AF_INET Stream defaultProtocol
     setSocketOption sock ReuseAddr 1
-    loopbackIp <- inet_addr "127.0.0.1"
+    loopbackIp <- inet_addr "0.0.0.0"
     bindSocket sock $
-        SockAddrInet 8080 loopbackIp
+        SockAddrInet 9000 loopbackIp
     listen sock (max 1024 maxListenQueue)
 
     httpTid <- forkIO $ simpleHTTPWithSocket' unpackErrorT
